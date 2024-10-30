60c2f58d41f5a06297854b38c798dda95a5b3b51
Felix Jung <felix.jung@tu-dresden.de>
2024-10-30 17:46:11 +0100

    some convenience stuff

--- src/scan_sorter/sorting.py
+++ src/scan_sorter/sorting.py
@@ -22,7 +22,7 @@ def n(i, N):
 
 
 def get_ordered_sequence(N: int):
-    assert not N % 2, "N must be even"
+    assert not N % 2, f"Number of pages must be even, but was {N=}"
     return [n(i, N) - 1 for i in range(0, N)]
 
 
@@ -42,13 +42,24 @@ def reorder_all_pdfs_in_directory(input_dir: Path, output_dir: Path):
         print("No PDFs in current directory. Done.")
         return
     n_skipped = 0
+    errors = []
     # for input_path in track(input_paths, "Processing..."):
     for input_path in tqdm(input_paths):
         output_path = output_dir / input_path.name
         if output_path.exists():
             n_skipped += 1
         else:
-            reorder_pdf(input_path=input_path, output_path=output_path)
+            try:
+                reorder_pdf(input_path=input_path, output_path=output_path)
+            except Exception as e:
+                errors.append(f'Reordering of {input_path.name} failed, error was "{e}"')
     print(
-        f"{len(input_paths)} files processed, {n_skipped} skipped due to output file existing."
+        f"\n{len(input_paths)} files processed, "
+        f"{n_skipped} skipped due to output file existing."
     )
+    if errors:
+        print(f"\n{len(errors)} file(s) failed processing. See details below:")
+        print("\n".join(errors))
+        print()
+
+
